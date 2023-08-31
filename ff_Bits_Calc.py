57618601e59d16f8bb72e6e616705063298f58ed
Morprok <141086733+Morprok@users.noreply.github.com>
2023-09-01 10:23:58 +1200

    Add files via upload

--- ff_Bits_Calc.py
+++ ff_Bits_Calc.py
@@ -71,6 +71,65 @@ def num_check(question, low):
             print(error)
 
 
+# TEST
+# calculates the # of bits for text (# of characters x 8)
+def text_bits():
+    print()
+    # ask user for string . . .
+    var_text = input("Enter some text: ")
+
+    # calculate # of bits (length of string x 8)
+    var_length = len(var_text)
+    num_bits = 8 * var_length
+
+    print()
+    print(f"\'{var_text}\' has {var_length} characters . . . ")
+    print(f"# of bits is {var_length} x 8 . . . ")
+    print(f"We need {num_bits} bits to represent {var_text}")
+    print()
+
+    return ""
+
+
+# TEST
+# calculates amount of bits in image
+def image_bits():
+    # get width and height . . .
+    image_width = num_check("Image width? ", 1)
+    image_height = num_check("Image height? ", 1)
+
+    # calculate # pixels
+    num_pixels = image_width * image_height
+
+    # calculate # bits (24 x num pixels)
+    num_bits = num_pixels * 24
+
+    # output answer with working
+    print()
+    print(f"# of pixels = {image_height} x {image_width} = {num_pixels}")
+    print(f"# bits = {num_pixels} x 24 = {num_bits}")
+    print()
+
+    return ""
+
+
+# TEST
+# converts decimal to binary and states how
+def int_bits():
+    # get integer (must be >= 0)
+    var_integer = num_check("Please enter an integer: ", 0)
+
+    var_binary = "{0:b}".format(var_integer)
+
+    # calculate # of bits (length of string above)
+    num_bits = len(var_binary)
+
+    # output answer with working
+    print()
+    print(f"{var_integer} in binary is {var_binary}")
+    print(f"# of bits is {num_bits}")
+
+    return ""
 
 
 # Main routine goes here
@@ -88,14 +147,16 @@ while keep_going == "":
 
     # For integers, ask for integer
     if data_type == "integer":
-        var_integer = num_check("Enter an integer: ", 0)
+        int_bits()
 
     # For images, ask for width and height
     # (must be integers more than / equals to 1)
     elif data_type == "image":
-        image_width = num_check("Image width? ", 1)
-        print()
-        image_height = num_check("Image height? ", 1)
+        image_bits()
 
     else:
-        var_text = input("Enter some text: ")
+        text_bits()
+
+    print()
+    keep_going = input("Press <enter> to continue or any key to quit ")
+    print()
